932a60691961d2b98f95139b1453898d9a013523
Gabrijel Boduljak <gboduljak@outlook.com>
2020-06-15 03:36:35 +0200

    removed unused code

--- prototype/utils.py
+++
@@ -1,9 +0,0 @@
-from numpy import array, rot90
-from PIL import Image
-
-
-def transpose_image(image: Image) -> Image:
-    image_bytes = array(image)
-    img = rot90(image_bytes, 1, (0, 1))
-    img = crop_c(img, scale_r)
-    img = rot90(image_bytes, 3, (0, 1))
